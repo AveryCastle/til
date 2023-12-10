67ea241ed46b6b5a0da9dde8246b62fc2ee9abc0
AveryCastle <estel.castle@gmail.com>
2023-12-10 14:06:03 +0900

    연습문제 6.6 풀기.

---
+++ kotlin/fpinkotlin/src/test/kotlin/chapter06/exercise06/solution.kt
@@ -0,0 +1,40 @@
+package chapter06.exercise06
+
+import chapter06.rng1
+import chapter06.sec04.Rand
+import chapter06.sec04.unit
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+//typealias Rand<A> = (RNG) -> Pair<A, RNG>
+
+//fun <A, B> map(s: Rand<A>, f: (A) -> B): Rand<B> =
+//    { rng ->
+//        val (a, rng2) = s(rng)
+//        f(a) to rng2
+//    }
+
+fun <A, B, C> map2(
+    ra: Rand<A>,
+    rb: Rand<B>,
+    f: (A, B) -> C
+): Rand<C> = { rng ->
+    val (a, rng2) = ra(rng)
+    val (b, rng3) = rb(rng2)
+    f(a, b) to rng3
+}
+
+class Solution6 : WordSpec({
+
+    "map2" should {
+        "combine the results of two actions" {
+
+            val combined: Rand<String> =
+                map2(unit(1.0), unit(1)) { d, i ->
+                    ">>> $d double; $i int"
+                }
+
+            combined(rng1).first shouldBe ">>> 1.0 double; 1 int"
+        }
+    }
+})
