c297f8b9e196eb80b72f1ef5613fe40bac2a3e6a
estel <estel.castle@gmail.com>
2023-12-14 15:32:52 +0900

    7.3 표현 선택하기.

---
+++ kotlin/fpinkotlin/src/main/kotlin/chapter07/sec02/listing.kt
@@ -0,0 +1,24 @@
+package chapter07.sec02
+
+import chapter07.sec01.Par
+import java.util.concurrent.TimeUnit
+
+interface Callable<A> {
+    fun call(): A
+}
+
+interface Future<A> {
+    fun get(): A
+    fun get(timeout: Long, timeUnit: TimeUnit)
+    fun cancel(evenIfRunning: Boolean): Boolean
+    fun isDone(): Boolean
+    fun isCancelled(): Boolean
+}
+
+interface ExecutorService {
+    fun <A> submit(a: Callable<A>): Future<A>
+}
+
+fun <A> run(ex: ExecutorService, a: Par<A>): A = TODO()
+
+typealias Par<A> = (ExecutorService) -> Future<A>
