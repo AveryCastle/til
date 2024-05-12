edf32c9667d6159061c1538f4926df199878a329
AveryCastle <estel.castle@gmail.com>
2024-05-12 23:13:09 +0900

    refact : invoice 데이터를 중각 데이터에 복사하기.

--- book/refactoring2/ch01/invoice.js
+++ book/refactoring2/ch01/invoice.js
@@ -1,68 +1,69 @@
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  return renderPlainText(statementData, invoice, plays);
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 
-  function renderPlainText(data, invoice, plays) {
+  function renderPlainText(data, plays) {
     let result = `청구 내역(고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
 
     result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
-  }
 
-  function totalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
+    function totalAmount() {
+      let result = 0;
+      for (let perf of data.performances) {
+        result += amountFor(perf);
+      }
+      return result;
     }
-    return result;
-  }
 
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += volumeCreditsFor(perf);
+    function totalVolumeCredits() {
+      let result = 0;
+      for (let perf of data.performances) {
+        result += volumeCreditsFor(perf);
+      }
+      return result;
     }
-    return result;
-  }
 
-  function volumeCreditsFor(perf) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if ("comedy" == playFor(perf).type)
-      result += Math.floor(perf.audience / 5);
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy": // 코믹
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    function volumeCreditsFor(perf) {
+      let result = 0;
+      result += Math.max(perf.audience - 30, 0);
+      if ("comedy" == playFor(perf).type)
+        result += Math.floor(perf.audience / 5);
+      return result;
     }
-    return result;
+
+    function playFor(aPerformance) {
+      return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+      let result = 0;
+      switch (playFor(aPerformance).type) {
+        case "tragedy": // 비극
+          result = 40000;
+          if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+          }
+          break;
+        case "comedy": // 코믹
+          result = 30000;
+          if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+          }
+          result += 300 * aPerformance.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+      }
+      return result;
+    }
   }
 }
 
