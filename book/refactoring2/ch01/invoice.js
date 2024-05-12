1d4f9a6b7538f60c6213fc62196d2a8f76bf21e4
AveryCastle <estel.castle@gmail.com>
2024-05-12 22:15:55 +0900

    refact : amountFor() 함수 추출하기.

--- book/refactoring2/ch01/invoice.js
+++ book/refactoring2/ch01/invoice.js
@@ -10,23 +10,7 @@ function statement(invoice, plays) {
     const play = plays[perf.playID];
     let thisAmount = 0;
 
-    switch (play.type) {
-      case "tragedy": // 비극
-        thisAmount = 40_000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy": // 코믹
-        thisAmount = 30_000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    thisAmount = amountFor(perf, play);
     // 포인트 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
@@ -38,6 +22,28 @@ function statement(invoice, plays) {
   result += `총액: ${format(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+}
+
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy": // 비극
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy": // 코믹
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return thisAmount;
 }
 
 module.exports = { statement };
