a1bc895e3724173cdf59be31e58af782a67bb568
AveryCastle <estel.castle@gmail.com>
2024-05-12 22:32:15 +0900

    refact : variable inline 적용하기.

--- book/refactoring2/ch01/invoice.js
+++ book/refactoring2/ch01/invoice.js
@@ -7,13 +7,12 @@ function statement(invoice, plays) {
                               minimumFractionDigits: 2 }).format;
                       
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
     // 포인트 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ("comedy" == playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
-    result += `  ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-    totalAmount += thisAmount;
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+    totalAmount += amountFor(perf);
   }
 
   result += `총액: ${format(totalAmount/100)}\n`;
