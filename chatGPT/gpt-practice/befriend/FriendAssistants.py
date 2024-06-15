dfdcceb923a96453729907569f98734a94e7099e
AveryCastle <estel.castle@gmail.com>
2024-06-15 16:57:19 +0900

    feat: email, password 3회 이상 틀리면 종료되도록 하기.

---
+++ chatGPT/gpt-practice/befriend/FriendAssistants.py
@@ -0,0 +1,46 @@
+import os
+from openai import OpenAI
+import json
+
+class FrenAssistant:
+    def __init__(self, thread_id=None):
+        api_key = os.getenv("OPENAI_API_KEY")
+        self.client = OpenAI(api_key=api_key)
+                
+        # 생성한 assistant 가져오기
+        self.assistant_id = 'asst_YaAriwexxjH8XhKZE3nXwoBk'
+        self.assistant = self.client.beta.assistants.retrieve(assistant_id=self.assistant_id)
+        
+        # thread 생성하기
+        if thread_id:
+            self.thread = self.client.beta.threads.retrieve(thread_id=thread_id)
+        else:
+            self.thread = self.client.beta.threads.create()
+
+    def get_thread_id(self):
+        return self.thread.id
+        
+    def ask_question(self, question):
+        # message 를 쓰레드에 추가하기
+        message = self.client.beta.threads.messages.create(
+            thread_id=self.thread.id,
+            role="user",
+            content=question
+        )
+
+        # run 생성 및 실행 
+        run = self.client.beta.threads.runs.create_and_poll(
+            thread_id=self.thread.id,
+            assistant_id=self.assistant.id,
+        )
+        return self.__get_response__(run)
+      
+    def __get_response__(self, run):
+        if run.status == 'completed': 
+            messages = self.client.beta.threads.messages.list(
+                thread_id=self.thread.id
+            )
+            return messages.data[0].content[0].text.value
+        else:
+            return run.status
+    
---
+++ chatGPT/gpt-practice/befriend/database_manager.py
@@ -0,0 +1,29 @@
+import sqlite3
+from sqlite3 import Error
+
+class DatabaseManager:
+    def __init__(self, db_name='befriends.db'):
+        self.conn = self.create_connection(db_name)
+        self.create_table()
+
+    def create_connection(self, db_name):
+        conn = None
+        try:
+            conn = sqlite3.connect(db_name)
+            print(sqlite3.version)
+        except Error as e:
+            print(e)
+        return conn
+
+    def create_table(self):
+        try:
+            sql_create_user_table = """ CREATE TABLE IF NOT EXISTS fren_users (
+                                            id integer PRIMARY KEY,
+                                            email text NOT NULL UNIQUE,
+                                            password text NOT NULL,
+                                            thread_id text
+                                        ); """
+            c = self.conn.cursor()
+            c.execute(sql_create_user_table)
+        except Error as e:
+            print(e)
