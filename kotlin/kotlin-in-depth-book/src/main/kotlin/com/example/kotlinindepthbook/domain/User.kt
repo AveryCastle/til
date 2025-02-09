6d549b803112f302d70f69af3e2aee2534939398
AveryCastle <estel.castle@gmail.com>
2025-02-09 11:48:54 +0900

    [Kotlin-Spring] feat: User 도메인 만들기.

---
+++ kotlin/kotlin-in-depth-book/src/main/kotlin/com/example/kotlinindepthbook/domain/User.kt
@@ -0,0 +1,34 @@
+package com.example.kotlinindepthbook.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
+@Entity
+class User(
+    @Id
+    val id: Long,
+
+    @Column(nullable = false)
+    var username: String,
+
+    @Column(nullable = false)
+    var age: Int,
+
+    @Column(nullable = false)
+    var gender: Gender,
+
+    @Column(nullable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now()
+)
+
+enum class Gender(val code: Int) {
+    MALE(1), FEMALE(2);
+
+    companion object {
+        fun fromCode(code: Int): Gender {
+            return entries.first { it.code == code }
+        }
+    }
+}
